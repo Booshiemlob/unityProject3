3a3ca582db2a32f358fee11df683d78fddd819ed
BuildTools <unconfigured@null.spigotmc.org>
2019-07-18 00:38:27 +0800

    a

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    //TODO Expand on the stats contained below, make sure you have some that are class-specific.
+    public string myName;
+    public Sprite mySprite;
+    public int health;
+    public int damage;
+    public int armour;
+    public int magicResistiance;
+    public int spellDamage;
+
+
+
+    #region VFX & SFX
+    public void ShowDamage()
+    {
+        GetComponent<AudioSource>().Play();
+        GetComponent<SpriteRenderer>().color = Color.red;
+        Invoke("StopDamage", 0.2f);
+    }
+
+    void StopDamage()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+    #endregion
+}
